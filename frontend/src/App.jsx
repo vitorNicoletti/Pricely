b34b9ccfdab6ac790a7c0ee592f12b2b0a79c167
Vini-y <vini.yama13@gmail.com>
2025-05-20 16:15:41 -0300

    Pagina de Login estruturada e com css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Login from './components/Login';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        Projeto Limpo
+      <div className="App">
+      <Login />
       </div>
     </>
   )
