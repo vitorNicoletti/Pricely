49bbd1045d7f0a82f402dfbd60d55d3ffbdabc8d
Leonardo Saito <leosaito632@gmail.com>
2025-05-22 17:21:56 -0300

    logo header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Login from './components/Login';
+import Header from './components/Header/Header'
 
 function App() {
-
   return (
     <>
       <div className="App">
-      <Login />
+      <Header/>
+      {/* <Login /> */}
       </div>
     </>
   )
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import style from "./Header.module.css"
+import logo_img from "../../assets/logo.png"
 
 function Header() {
   return (
     <div className={style.header}>
-      <div className={style.logo}></div>
+      <div className={style.logo_div}>
+        <img className={style.logo_img} src={logo_img} alt="logo" />
+      </div>
       <div className={style.navbar}></div>
       <div className={style.icones}></div>
     </div>
