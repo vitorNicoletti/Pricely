eafaee1f44d3615084b6522b4374ff84cbc50626
Leonardo Saito <leosaito632@gmail.com>
2025-05-22 20:22:47 -0300

    base header feita

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,9 @@ function App() {
 
   return (
     <>
+      <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
       <div>
-        
       </div>
     </>
   )
