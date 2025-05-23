6cb7c89e3a829fd39a4d81108b65903a70cc9bf4
Leonardo Saito <85519355+Leosaito632@users.noreply.github.com>
2025-05-23 18:27:55 -0300

    Payment (#4)
    * footer feito
    
    * Pagina de pagamento feita igual ao figma
    
    * Verificações para os campos
    
    * Adicionei alguns paises
    
    * Arrumei a margem do numero do cartao
    
    * Adicionei alguns paises e coloquei a cor da borda na mesma que  os primeiros botoes
    
    * .
    
    * fixing project structure
    
    ---------
    
    Co-authored-by: Bruno <manobrunodutra@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-
 import { useState } from 'react'
 import './App.css'
 import Login from './components/Login/Login.jsx';
@@ -7,11 +6,13 @@ import Header from './components/Header/Header'
 
 import Footer from './components/Footer/Footer.jsx'
 
+
+import Payment from './components/Payment/Payment.jsx'
+
 function App() {
 
   return (
     <>
-
       <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
       <div>
