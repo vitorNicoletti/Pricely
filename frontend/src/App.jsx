778680abaff57156c0395820f6900311e2444c73
Leonardo Saito <leosaito632@gmail.com>
2025-05-23 17:19:39 -0300

    removing tests components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,13 @@
 import './App.css'
-import ComponentTest from './pages/ComponentTest'
-
 
 function App() {
 
   return (
     <>
-
       <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
-      <ComponentTest/>
+      <div>
+      </div>
     </>
   )
 }
