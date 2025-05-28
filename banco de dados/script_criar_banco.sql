b13c9538d36d2196b7c789f62def81501c25c0b8
Mohamad Diab <mohamadkd14@hotmail.com>
2025-05-28 20:36:43 -0300

    Banco concertado e com Imagem em BASE64

--- banco de dados/script_criar_banco.sql
+++ banco de dados/script_criar_banco.sql
@@ -7,7 +7,6 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 
 CREATE SCHEMA IF NOT EXISTS `Pricely` DEFAULT CHARACTER SET utf8 ;
-CREATE SCHEMA IF NOT EXISTS `pricely` DEFAULT CHARACTER SET utf8mb3 ;
 USE `Pricely` ;
 
 -- -----------------------------------------------------
@@ -20,9 +19,17 @@ CREATE TABLE IF NOT EXISTS `Pricely`.`usuario` (
   `salt` VARCHAR(200) NULL,
   `dataCadastro` DATETIME NOT NULL,
   `telefone` VARCHAR(45) NULL,
-  PRIMARY KEY (`id_usuario`))
-ENGINE = InnoDB;
-
+  `perfil_arquivo_id` INT NULL,
+  `documento_arquivo_id`     INT NULL,
+  PRIMARY KEY (`id_usuario`),
+  CONSTRAINT `fk_usuario_perfil_arquivo`
+    FOREIGN KEY (`perfil_arquivo_id`)
+    REFERENCES `Pricely`.`arquivos` (`id`),
+  CONSTRAINT `fk_usuario_documento_arquivo`
+    FOREIGN KEY (`documento_arquivo_id`)
+    REFERENCES `Pricely`.`arquivos` (`id`)
+) ENGINE=InnoDB
+  DEFAULT CHARSET=utf8mb4;
 
 -- -----------------------------------------------------
 -- Table `Pricely`.`seguindo`
@@ -45,7 +52,25 @@ CREATE TABLE IF NOT EXISTS `Pricely`.`seguindo` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
+-- -----------------------------------------------------
+-- Table `Pricely`.`carteira`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Pricely`.`carteira` (
+  `id_carteira`        INT            NOT NULL AUTO_INCREMENT,
+  `saldo`              DECIMAL(15,2)  NOT NULL DEFAULT 0.00,
+  `ultima_atualizacao` DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP
+                                           ON UPDATE CURRENT_TIMESTAMP,
+  `id_usuario`         INT            NOT NULL,
+  PRIMARY KEY (`id_carteira`),
+  KEY `idx_carteira_usuario` (`id_usuario`),
+  CONSTRAINT `fk_carteira_usuario`
+    FOREIGN KEY (`id_usuario`)
+    REFERENCES `Pricely`.`usuario` (`id_usuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+  
 -- -----------------------------------------------------
 -- Table `Pricely`.`vendedor`
 -- -----------------------------------------------------
@@ -59,7 +84,6 @@ CREATE TABLE IF NOT EXISTS `Pricely`.`vendedor` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
-
 
 -- -----------------------------------------------------
 -- Table `Pricely`.`fornecedor`
@@ -86,25 +110,6 @@ CREATE TABLE IF NOT EXISTS `Pricely`.`fornecedor` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
--- -----------------------------------------------------
--- Table `Pricely`.`url_documento`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Pricely`.`url_documento` (
-  `id_doc` INT NOT NULL,
-  `dataCadastro` DATETIME NOT NULL,
-  `link` VARCHAR(200) NOT NULL,
-  `id_fornecedor` INT NOT NULL,
-  PRIMARY KEY (`id_doc`),
-  INDEX `fk_url_documento_1_idx` (`id_fornecedor` ASC),
-  CONSTRAINT `fk_url_documento_1`
-    FOREIGN KEY (`id_fornecedor`)
-    REFERENCES `Pricely`.`fornecedor` (`id_usuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
 -- -----------------------------------------------------
 -- Table `Pricely`.`pedido`
 -- -----------------------------------------------------
@@ -127,26 +132,26 @@ CREATE TABLE IF NOT EXISTS `Pricely`.`pedido` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
 -- -----------------------------------------------------
 -- Table `Pricely`.`produto`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Pricely`.`produto` (
-  `id_produto` INT NOT NULL,
-  `nome` VARCHAR(300) NOT NULL,
-  `descricao` VARCHAR(1000) NULL,
-  `preco_unidade` DOUBLE NOT NULL,
-  `estado` VARCHAR(50) NULL,
-  `id_fornecedor` INT NOT NULL,
+  `id_produto`       INT      NOT NULL,
+  `nome`             VARCHAR(300)  NOT NULL,
+  `descricao`        VARCHAR(1000) NULL,
+  `preco_unidade`    DOUBLE   NOT NULL,
+  `estado`           VARCHAR(50)   NULL,
+  `id_fornecedor`    INT      NOT NULL,
+  `imagem_arquivo_id` INT     NULL,
   PRIMARY KEY (`id_produto`),
-  INDEX `fk_produto_1_idx` (`id_fornecedor` ASC),
-  CONSTRAINT `fk_produto_1`
+  CONSTRAINT `fk_produto_fornecedor`
     FOREIGN KEY (`id_fornecedor`)
-    REFERENCES `Pricely`.`fornecedor` (`id_usuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
+    REFERENCES `Pricely`.`fornecedor` (`id_usuario`),
+  CONSTRAINT `fk_produto_imagem_arquivo`
+    FOREIGN KEY (`imagem_arquivo_id`)
+    REFERENCES `Pricely`.`arquivos` (`id`)
+) ENGINE=InnoDB
+  DEFAULT CHARSET=utf8mb4;
 
 -- -----------------------------------------------------
 -- Table `Pricely`.`avaliacao`
@@ -338,274 +343,17 @@ CREATE TABLE IF NOT EXISTS `Pricely`.`info_bancaria` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-USE `pricely` ;
-
 -- -----------------------------------------------------
--- Table `pricely`.`avaliacao`
+-- Table `Pricely`.`arquivos` 
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pricely`.`avaliacao` (
-  `id_avaliacao` INT NOT NULL,
-  `texto_avaliacao` VARCHAR(500) NULL DEFAULT NULL,
-  `avaliacao` TINYINT NOT NULL,
-  PRIMARY KEY (`id_avaliacao`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `pricely`.`usuario`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pricely`.`usuario` (
-  `id_usuario` INT NOT NULL,
-  `email` VARCHAR(100) NOT NULL,
-  `senha` VARCHAR(200) NOT NULL,
-  `salt` VARCHAR(200) NULL DEFAULT NULL,
-  `dataCadastro` DATETIME NOT NULL,
-  `telefone` VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (`id_usuario`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `pricely`.`fornecedor`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pricely`.`fornecedor` (
-  `id_usuario` INT NOT NULL,
-  `razao_social` VARCHAR(200) NOT NULL,
-  `nome_fantasia` VARCHAR(200) NULL DEFAULT NULL,
-  `cnpj` VARCHAR(14) NOT NULL,
-  `inscricao_estadual` VARCHAR(45) NULL DEFAULT NULL,
-  `inscricao_municipal` VARCHAR(45) NULL DEFAULT NULL,
-  `logradouro` VARCHAR(150) NOT NULL,
-  `numero` VARCHAR(10) NOT NULL,
-  `complemento` VARCHAR(150) NULL DEFAULT NULL,
-  `rep_nome` VARCHAR(200) NULL DEFAULT NULL,
-  `rep_cpf` VARCHAR(12) NULL DEFAULT NULL,
-  `rep_telefone` VARCHAR(15) NULL DEFAULT NULL,
-  PRIMARY KEY (`id_usuario`),
-  UNIQUE INDEX `CNPJ_UNIQUE` (`cnpj` ASC),
-  CONSTRAINT `fk_fornecedor_usuario`
-    FOREIGN KEY (`id_usuario`)
-    REFERENCES `pricely`.`usuario` (`id_usuario`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `pricely`.`avaliacao_fornecedor`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pricely`.`avaliacao_fornecedor` (
-  `id_avaliacao` INT NOT NULL,
-  `id_fornecedor` INT NOT NULL,
-  PRIMARY KEY (`id_avaliacao`),
-  INDEX `idx_av_fornecedor` (`id_fornecedor` ASC),
-  CONSTRAINT `fk_av_fornecedor_av`
-    FOREIGN KEY (`id_avaliacao`)
-    REFERENCES `pricely`.`avaliacao` (`id_avaliacao`),
-  CONSTRAINT `fk_av_fornecedor_forn`
-    FOREIGN KEY (`id_fornecedor`)
-    REFERENCES `pricely`.`fornecedor` (`id_usuario`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `pricely`.`produto`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pricely`.`produto` (
-  `id_produto` INT NOT NULL,
-  `nome` VARCHAR(300) NOT NULL,
-  `descricao` VARCHAR(1000) NULL DEFAULT NULL,
-  `preco_unidade` DOUBLE NOT NULL,
-  `estado` VARCHAR(50) NULL DEFAULT NULL,
-  `id_fornecedor` INT NOT NULL,
-  PRIMARY KEY (`id_produto`),
-  INDEX `idx_produto_fornecedor` (`id_fornecedor` ASC),
-  CONSTRAINT `fk_produto_fornecedor`
-    FOREIGN KEY (`id_fornecedor`)
-    REFERENCES `pricely`.`fornecedor` (`id_usuario`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `pricely`.`avaliacao_produto`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pricely`.`avaliacao_produto` (
-  `id_avaliacao` INT NOT NULL,
-  `id_produto` INT NOT NULL,
-  PRIMARY KEY (`id_avaliacao`),
-  INDEX `idx_av_produto` (`id_produto` ASC),
-  CONSTRAINT `fk_av_produto_av`
-    FOREIGN KEY (`id_avaliacao`)
-    REFERENCES `pricely`.`avaliacao` (`id_avaliacao`),
-  CONSTRAINT `fk_av_produto_prod`
-    FOREIGN KEY (`id_produto`)
-    REFERENCES `pricely`.`produto` (`id_produto`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `pricely`.`carteira`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pricely`.`carteira` (
-  `id_carteira` INT NOT NULL AUTO_INCREMENT,
-  `saldo` DECIMAL(15,2) NOT NULL DEFAULT '0.00',
-  `ultima_atualizacao` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `id_usuario` INT NOT NULL,
-  PRIMARY KEY (`id_carteira`),
-  INDEX `fk_carteira_usuario1_idx` (`id_usuario` ASC),
-  CONSTRAINT `fk_carteira_usuario1`
-    FOREIGN KEY (`id_usuario`)
-    REFERENCES `Pricely`.`usuario` (`id_usuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `pricely`.`compra`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pricely`.`compra` (
-  `id_compra` INT NOT NULL,
-  `preco_unidade` DOUBLE NOT NULL,
-  `quantidade` INT NOT NULL,
-  `frete_pago` DOUBLE NULL DEFAULT NULL,
-  `estado` VARCHAR(50) NULL DEFAULT NULL,
-  `id_produto` INT NOT NULL,
-  `id_pedido` INT NOT NULL,
-  `id_avaliacao_fornecedor` INT NULL DEFAULT NULL,
-  `id_avaliacao_produto` INT NULL DEFAULT NULL,
-  `id_conjunto` INT NULL DEFAULT NULL,
-  PRIMARY KEY (`id_compra`),
-  INDEX `idx_compra_produto` (`id_produto` ASC),
-  CONSTRAINT `fk_compra_produto`
-    FOREIGN KEY (`id_produto`)
-    REFERENCES `pricely`.`produto` (`id_produto`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `pricely`.`conjunto`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pricely`.`conjunto` (
-  `id_conjunto` INT NOT NULL,
-  `frete_max` DOUBLE NULL DEFAULT NULL,
-  `raio_metros` DOUBLE NOT NULL,
-  `longitude` VARCHAR(100) NOT NULL,
-  `latitude` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`id_conjunto`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `pricely`.`oferta`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pricely`.`oferta` (
-  `id_oferta` INT NOT NULL,
-  `dataCadastro` DATETIME NOT NULL,
-  `id_fornecedor` INT NOT NULL,
-  PRIMARY KEY (`id_oferta`),
-  INDEX `idx_oferta_fornecedor` (`id_fornecedor` ASC),
-  CONSTRAINT `fk_oferta_fornecedor`
-    FOREIGN KEY (`id_fornecedor`)
-    REFERENCES `pricely`.`fornecedor` (`id_usuario`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `pricely`.`grupo_promocao`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pricely`.`grupo_promocao` (
-  `idgrupo_promocao` INT NOT NULL,
-  `quantidade` INT NOT NULL,
-  `desc_porcentagem` INT NOT NULL,
-  `id_oferta` INT NOT NULL,
-  PRIMARY KEY (`idgrupo_promocao`),
-  INDEX `idx_gp_oferta` (`id_oferta` ASC),
-  CONSTRAINT `fk_gp_oferta`
-    FOREIGN KEY (`id_oferta`)
-    REFERENCES `pricely`.`oferta` (`id_oferta`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `pricely`.`info_bancaria`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pricely`.`info_bancaria` (
-  `id_info_banco` INT NOT NULL,
-  `banco` VARCHAR(100) NOT NULL,
-  `agencia` VARCHAR(50) NOT NULL,
-  `conta` VARCHAR(50) NOT NULL,
-  `tipo_conta` VARCHAR(50) NOT NULL,
-  `pix` VARCHAR(50) NULL DEFAULT NULL,
-  `id_user` INT NOT NULL,
-  PRIMARY KEY (`id_info_banco`),
-  INDEX `idx_banco_user` (`id_user` ASC),
-  CONSTRAINT `fk_banco_user`
-    FOREIGN KEY (`id_user`)
-    REFERENCES `pricely`.`usuario` (`id_usuario`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `pricely`.`produto_oferta`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pricely`.`produto_oferta` (
-  `id_oferta` INT NOT NULL,
-  `id_produto` INT NOT NULL,
-  PRIMARY KEY (`id_oferta`, `id_produto`),
-  INDEX `fk_po_produto` (`id_produto` ASC),
-  CONSTRAINT `fk_po_oferta`
-    FOREIGN KEY (`id_oferta`)
-    REFERENCES `pricely`.`oferta` (`id_oferta`),
-  CONSTRAINT `fk_po_produto`
-    FOREIGN KEY (`id_produto`)
-    REFERENCES `pricely`.`produto` (`id_produto`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `pricely`.`seguindo`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pricely`.`seguindo` (
-  `id_usuario_seguidor` INT NOT NULL,
-  `id_usuario_seguido` INT NOT NULL,
-  `dataCadastro` DATETIME NOT NULL,
-  PRIMARY KEY (`id_usuario_seguidor`, `id_usuario_seguido`),
-  INDEX `idx_seg_seguido` (`id_usuario_seguido` ASC),
-  CONSTRAINT `fk_seg_seguido`
-    FOREIGN KEY (`id_usuario_seguido`)
-    REFERENCES `pricely`.`usuario` (`id_usuario`),
-  CONSTRAINT `fk_seg_seguidor`
-    FOREIGN KEY (`id_usuario_seguidor`)
-    REFERENCES `pricely`.`usuario` (`id_usuario`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `pricely`.`vendedor`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pricely`.`vendedor` (
-  `id_usuario` INT NOT NULL,
-  `cpfCnpj` VARCHAR(14) NOT NULL,
-  PRIMARY KEY (`id_usuario`),
-  CONSTRAINT `fk_vendedor_usuario`
-    FOREIGN KEY (`id_usuario`)
-    REFERENCES `pricely`.`usuario` (`id_usuario`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
+CREATE TABLE IF NOT EXISTS `Pricely`.`arquivos` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NOT NULL,
+  `tipo` VARCHAR(100) NOT NULL,
+  `dados` LONGTEXT NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB
+  DEFAULT CHARSET=utf8mb4;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
