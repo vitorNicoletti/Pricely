fa767f1aa314841255950a950ac8348bb79dfc6d
Leonardo Saito <85519355+Leosaito632@users.noreply.github.com>
2025-05-28 22:51:24 -0300

    cart and details buttons (#9)
    tô aceitando meu próprio pull request porque eu posso, se o Brunão fizer a mesma coisa ele tá errado

--- frontend/src/components/ProductCard/ProductCard.jsx
+++ frontend/src/components/ProductCard/ProductCard.jsx
@@ -5,7 +5,7 @@ function ProductCard({ product }) {
     <div className={style.card}>
       <div className={style.img_div}>
         <img
-          src={product.image || 'https://via.placeholder.com/150'}
+          src={product.image || "https://via.placeholder.com/150"}
           alt={`Imagem de ${product.title}`}
           className={style.img_produto}
         />
@@ -20,16 +20,21 @@ function ProductCard({ product }) {
             <i className="fa-solid fa-location-dot" /> {product.location}
           </p>
           <p>
-            <i className="fa-solid fa-percent" /> {product.discount ? 'Com desconto' : 'Sem descontos'}
+            <i className="fa-solid fa-percent" />{" "}
+            {product.discount ? "Com desconto" : "Sem descontos"}
           </p>
         </div>
         <div>
           <h2>R$ {product.price.toFixed(2)}</h2>
           <p>por {product.unit}</p>
         </div>
-        <p className={style.fornecedor_text}>
-          Fornecedor genérico
-        </p>
+        <p className={style.fornecedor_text}>Fornecedor genérico</p>
+      </div>
+      <div className={style.btn_div}>
+        <button className={style.btn}>
+          <i class="fa-solid fa-cart-shopping" />
+        </button>
+        <a href="/detalhes" className={style.btn}>Ver Detalhes</a>
       </div>
     </div>
   );
