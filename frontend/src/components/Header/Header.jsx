a56605f77f8a191eebfebd6a5712853795fb01bd
Leonardo Saito <leosaito632@gmail.com>
2025-06-05 02:18:36 -0300

    cart routes and page created

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,25 +1,33 @@
-import style from "./Header.module.css"
-import logo_img from "../../assets/logo.png"
+import style from "./Header.module.css";
+import logo_img from "../../assets/logo.png";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
+
   return (
-
     <div className={style.header}>
-
       <div className={style.logo_div}>
-        <img className={style.logo_img} src={logo_img} alt="logo" />
+        <img
+          className={style.logo_img}
+          onClick={() => navigate("/")}
+          src={logo_img}
+          alt="logo"
+        />
       </div>
       <div className={style.navbar}>
         <a href="/">Home</a>
-        <a href="/shop">Shop</a>
         <a href="/about">Sobre</a>
         <a href="/help">Fale Conosco</a>
       </div>
       <div className={style.navbar}>
+        {/* TODO: Usar classes para os ícones no lugar dos botoes */}
         <i className="fa-regular fa-user" />
         <i className="fa-solid fa-magnifying-glass" />
         <i className="fa-regular fa-heart" />
-        <i className="fa-solid fa-cart-shopping" />
+        <button className={style.icons_btn} onClick={() => navigate("/cart")}>
+          <i className="fa-solid fa-cart-shopping" />
+        </button>
       </div>
     </div>
   );
