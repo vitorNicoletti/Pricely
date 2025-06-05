aa2411bdabddec235745837dc9bbb75986a8daac
Leonardo Saito <leosaito632@gmail.com>
2025-06-05 20:03:11 -0300

    modal with mock data

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import style from "./Header.module.css";
 import logo_img from "../../assets/logo.png";
 import { useNavigate } from "react-router-dom";
+import ProfileModal from "../ProfileModal/ProfileModal.jsx";
+import { useState } from "react";
 
 function Header() {
   const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <div className={style.header}>
@@ -22,12 +29,26 @@ function Header() {
       </div>
       <div className={style.navbar}>
         {/* TODO: Usar classes para os ícones no lugar dos botoes */}
-        <i className="fa-regular fa-user" />
-        <i className="fa-solid fa-magnifying-glass" />
-        <i className="fa-regular fa-heart" />
-        <button className={style.icons_btn} onClick={() => navigate("/cart")}>
-          <i className="fa-solid fa-cart-shopping" />
+        <div className={style.profileWrapper}>
+          <button className={style.icons_btn} onClick={toggleModal}>
+            <i className="fa-regular fa-user" />
+          </button>
+          {isOpen && (
+            <div className={style.modalWrapper}>
+              <ProfileModal
+                onClose={toggleModal}
+                user={{
+                  name: "João Silva",
+                  avatar: "",
+                }}
+              />
+            </div>
+          )}
+        </div>
+        <button className={style.icons_btn}>
+          <i className="fa-solid fa-magnifying-glass" />
         </button>
+
       </div>
     </div>
   );
