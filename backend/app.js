c9921f0289d3f1294b0af4cd12c4fb193e72a456
Leonardo Saito <85519355+Leosaito632@users.noreply.github.com>
2025-06-05 01:31:42 -0300

    Update app.js
    arrumeis os conflict errado, agora é pra ficar tudo certo

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,8 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const catalogoRouter = require('./routes/catalogo');
 const cadastroRouter = require('./routes/cadastro');
-const vendedorRoutes = require("./routes/vendedor");
-const fornecedorRoutes = require("./routes/fornecedor");
+const vendedorRouter = require("./routes/vendedor");
+const fornecedorRouter = require("./routes/fornecedor");
 const loginRouter = require('./routes/login');
 dotenv.config(); // Carrega variáveis do .env
 
@@ -14,9 +14,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", catalogoRoutes);
-app.use("/api/vendedor", vendedorRoutes);
-app.use("/api/fornecedor", fornecedorRoutes);
+app.use("/api", catalogoRouter);
+app.use("/api/vendedor", vendedorRouter);
+app.use("/api/fornecedor", fornecedorRouter);
 app.use('/api/cadastro',cadastroRouter )
 app.use('/api/login',loginRouter )
 
