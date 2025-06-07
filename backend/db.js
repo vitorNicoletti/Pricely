ae8b719bef44360e3d4817b819a8723c27eb601a
Leonardo Saito <leosaito632@gmail.com>
2025-06-07 19:56:54 -0300

    fix vendedor model using callback (should be with promisse)

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
-mysql = require('mysql2')
-require('dotenv').config();
+mysql = require("mysql2");
+require("dotenv").config();
 const db = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'pricely',
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "pricely",
 });
 
-module.exports = db
+module.exports = db;
